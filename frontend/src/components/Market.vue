c64c204dafc75bc3c149dea2d0b25e2f405ca2a6
Alvin Mei <alvinkmei@gmail.com>
2022-07-06 12:33:45 -0400

    boxes

--- frontend/src/components/Market.vue
+++ frontend/src/components/Market.vue
@@ -1,11 +1,13 @@
 <template>
 <div class="market-view">
-Hello
+Insert Plotly Graph here
 </div>
 </template>
 
 <style lang="scss">
 .market-view {
   border: 1px solid $foreground;
+  height: 40rem;
+  padding: 0.5rem;
 }
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
 import Market from '../components/Market.vue';
+import Items from '../components/Items.vue';
 </script>
 
 <template>
   <main class="screen">
     <Market/>
+    <div class="info">
+      <Items/> 
+    </div>
+    
   </main>
 </template>
+
+<style lang="scss">
+.screen {
+  width: 95%;
+  margin: auto;
+  margin-top: 1rem;
+}
+.info {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  margin-top: 1rem;
+}
+</style>
