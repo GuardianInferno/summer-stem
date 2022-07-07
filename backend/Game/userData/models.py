ecf87fee4ec7b840f7e84ea312d423adef796f18
Hashir <hashirabbasi112@gmail.com>
2022-07-07 11:49:42 -0400

    model

--- backend/Game/userData/models.py
+++ backend/Game/userData/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class userInfo(models.Model):
+    username = models.CharField(max_length=255)
+    money = models.DecimalField(max_digits=100,decimal_places=2)
+    time_played = models.DateTimeField()
