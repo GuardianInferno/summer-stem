dfacdb30eeae83ef2dbe95ee21ce6e5a678445c3
Hashir <hashirabbasi112@gmail.com>
2022-07-07 08:44:48 -0400

    auth packages

--- backend/Game/product/views.py
+++ backend/Game/product/views.py
@@ -5,8 +5,10 @@ from rest_framework.response import Response
 from django.http import Http404
 from .models import Product
 # Create your views here.
+
 class ProductList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
+        print("hello")
         return Response(serializer.data)
